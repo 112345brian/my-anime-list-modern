322a6ab78a6f48d66300325c91f660053da6ff36
112345brian <11creepman@gmail.com>
2026-05-04 10:41:32 -0700

    Refine news detail sidebar typography

--- mal_modern_companion.js
+++ mal_modern_companion.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MAL Modern Companion
 // @namespace    http://tampermonkey.net/
-// @version      6.0.7
+// @version      6.0.8
 // @description  Editorial news desk, hover previews, keyboard nav for MyAnimeList
 // @author       You
 // @downloadURL  https://raw.githubusercontent.com/112345brian/my-anime-list-modern/main/mal_modern_companion.js
